247534ea6781831cf277a61785df2b98cece6420
Obaidullah Arshad <obaidullahset@gmail.com>
2025-09-09 19:09:12 +0500

    3rd

--- frontend/src/components/nav/footer.svelte
+++ frontend/src/components/nav/footer.svelte
@@ -45,7 +45,7 @@
 			<!-- Footer links -->
 			<div class="flex items-center gap-2 lg:gap-4">
 				{#each footerLinks as link}
-					<a href={link.href} class="text-sm text-gray-500 transition-colors hover:text-gray-700">
+					<a href={link.href} class="text-md text-gray-500 transition-colors hover:text-gray-700">
 						{link.name}
 					</a>
 				{/each}
