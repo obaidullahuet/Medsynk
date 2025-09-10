aa36cd4d2a9054a96fb3f570c1b7efc767817f42
Obaidullah Arshad <obaidullahset@gmail.com>
2025-09-10 19:35:15 +0500

    ko

--- frontend/src/components/nav/patientsNavbar.svelte
+++ frontend/src/components/nav/patientsNavbar.svelte
@@ -91,7 +91,7 @@
 					class="ml-1 h-3.5 w-4 md:h-4 md:w-4"
 					fill="none"
 					stroke="currentColor"
-					viewBox="0 0 24 24"
+					viewBox="0 2 24 24"
 				>
 					<path
 						stroke-linecap="round"
