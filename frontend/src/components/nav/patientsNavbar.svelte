abce44108b62b51ecf8be1dc53f09547acb446b7
Obaidullah Arshad <obaidullahset@gmail.com>
2025-09-10 19:34:31 +0500

    chnges

--- frontend/src/components/nav/patientsNavbar.svelte
+++ frontend/src/components/nav/patientsNavbar.svelte
@@ -88,7 +88,7 @@
 			>
 				<span>Treatment</span>
 				<svg
-					class="ml-1 h-3.5 w-3.5 md:h-4 md:w-4"
+					class="ml-1 h-3.5 w-4 md:h-4 md:w-4"
 					fill="none"
 					stroke="currentColor"
 					viewBox="0 0 24 24"
