b93e89cf11f737b4c3025c7be59330dac34cb9d6
Obaidullah Arshad <obaidullahset@gmail.com>
2025-09-10 19:33:53 +0500

    ok

--- frontend/src/components/nav/patientsNavbar.svelte
+++ frontend/src/components/nav/patientsNavbar.svelte
@@ -84,7 +84,7 @@
 		<div class="relative hidden md:block">
 			<button
 				onclick={toggleCustomerDropdown}
-				class="hover:bg-opacity-70 btn-dropdown-color1 flex w-full items-center justify-between rounded-full px-3 py-2 text-xs font-medium md:text-xs lg:px-4 lg:py-2 lg:text-sm"
+				class="hover:bg-opacity-70 btn-dropdown-color1 flex w-full items-center justify-between rounded-full px-3 py-2 text-sm font-medium md:text-xs lg:px-4 lg:py-2 lg:text-sm"
 			>
 				<span>Treatment</span>
 				<svg
