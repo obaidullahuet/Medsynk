349c7979cef75aaff7eeff6ebda80612e8dd3e1d
Obaidullah Arshad <obaidullahset@gmail.com>
2025-09-11 19:19:25 +0500

    hh

--- frontend/src/components/nav/patientsNavbar.svelte
+++ frontend/src/components/nav/patientsNavbar.svelte
@@ -68,8 +68,8 @@
 				oninput={handleInput}
 			/>
 			<div class="flex gap-2">
-				<button class="btn-dropdown-color1 h-7 w-6 rounded-full text-base font-bold">+</button>
-				<button class="btn-dropdown-color1 h-4 w-6 rounded-full text-base font-bold">−</button>
+				<button class="btn-dropdown-color1 h-7 w-3 rounded-full text-base font-bold">+</button>
+				<button class="btn-dropdown-color1 h-7 w-3 rounded-full text-base font-bold">−</button>
 			</div>
 		</div>
 
